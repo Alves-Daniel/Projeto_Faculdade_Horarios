8b2639917652bf386b51f6b596c9cc0ad216a359
Daniel Oliveira Alves <39285325+Alves-Daniel@users.noreply.github.com>
2018-11-12 20:42:39 -0400

    Input no banco de dados mysql

--- index.php
+++ index.php
@@ -1,35 +1,19 @@
-<?php 
-// Realizamos a importação da biblioteca para gerar QR Code.
-require_once('phpqrcode/qrlib.php'); 
+<?php
 
-// Credenciais de acesso ao banco de dados.
-$servidor = "localhost";
-$usuario  = "root";
-$senha    = "";
-$dbname   = "horarios";
+?>
 
-// Realizamos a conexão com o banco de dados.
-$conn = new PDO(
-    'mysql:host='.$servidor.';dbname='.$dbname, $usuario, $senha);
+<!DOCTYPE html>
+<html lang='pt-br'>
+    <head>
+        <meta charset="uft-">
+        <title>Contato</title>
+    </head>
 
-// Construimos nossa query de consulta.
-$queryString = 'SELECT * FROM disciplinas';
-
-// execucamos a pesquisa, retornando todos os registros da tabela cadastro.
-$data = $conn->query($queryString);
-
-// Realizamos um laço de repetição para percorrer cada registro retornado do DB.
-foreach ($data as $value) {
-    // Configuramos um nome único para o QR Code.
-    $qrCodeName = "imagem_qrcode_{$value['disciplina']}.png";
-
-    /**
-     * Realizamos a criação da imagem PNG, sendo passado as seguintes informaões:
-     * 1º - A string que desejamos inserir no QR Code.
-     * 2º - O nome da imagem que criamos no passo anterior.
-     */
-    QRcode::png(  "Disciplina  ". "Professor  ". "Sala  ". "\n". $value['disciplina']." ".$value['professor']. " ". $value['sala'], $qrCodeName);
-    
-    // Para finalizar realizamos a exibição da imagem no navegador.
-    echo "<img src='{$qrCodeName}'>";
-}
+    <body>
+        <form method="POST" action="salva.php">
+            Disciplina: <input type="text" name="disciplina" placeholder="Nome da Disciplina" required><br>
+            Professor: <input type="text" name="professor" placeholder="Nome do Professor" required><br>
+            Sala: <input type="text" name="sala" placeholder="Sala onde Será a Aula" required><br><br><br>
+            <input type="submit" value="Enviar">
+    </body>
+<html>
